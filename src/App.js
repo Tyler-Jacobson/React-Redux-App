e08d803a267876cc12051dfe72381b5ca1132173
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-07 15:36:41 -0700

    MVP Almost Met. Needs styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
-function App() {
+import { fetchClasses } from "./actions/index";
+
+import "./App.css";
+
+function App(props) {
+  console.log(props.classes);
+  // const classes = props.classes.results
+  useEffect(() => {
+    // this effect will only call an action creator, not make an API call
+    props.fetchClasses();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Choose Your Class</h1>
+      {props.classes.map((item) => {
+        return <h3>{item.name}</h3>;
+      })}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    classes: state.classes,
+    isLoading: state.isLoading,
+  };
+};
+
+export default connect(mapStateToProps, { fetchClasses })(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+import { characterReducer } from "./reducers/characterReducer"
+
+const store = createStore(characterReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
